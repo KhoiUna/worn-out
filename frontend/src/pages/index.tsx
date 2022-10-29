da6a1d2800e3e535847441dfbf0476ba9d242bfb
KhoiNguyen <thorwaitson@gmail.com>
2022-10-29 18:29:10 -0500

    update global.css

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,49 @@
 import { Calendar } from "react-calendar";
 import { Link } from "react-router-dom";
+import Outfit from "../components/Outfit";
+import Layout from "../containers/Layout";
+import homeStyle from "../styles/home.module.css";
+
+type Outfit = {
+  id: string;
+  image_url: string;
+  outfit_name: string;
+};
+
+const outfits: Outfit[] = [
+  {
+    id: "fsafds",
+    image_url:
+      "https://www.boredpanda.com/blog/wp-content/uploads/2022/03/horrible-dresses-25-622749b4cfad9__700.jpg",
+    outfit_name: "sun god",
+  },
+  {
+    id: "asdfdasf",
+    image_url:
+      "https://i.pinimg.com/originals/6a/c3/fe/6ac3fe0fcc2108847c9e107a5b2305cd.jpg",
+    outfit_name: "What?",
+  },
+];
 
 const Index = () => {
   return (
-    <>
-      <h1>Worn Out</h1>
+    <Layout>
+      <div className={homeStyle.container}>
+        <Calendar />
+      </div>
 
-      <Calendar />
+      <div className={homeStyle.container}>
+        <input className={homeStyle.search} placeholder="Enter Details" />
+      </div>
 
-      <Link to={"/view"}>View</Link>
-    </>
+      <div className={homeStyle.outfit_container}>
+        {outfits.map((item) => (
+          <Link to={`/${item.id}`} key={item.id}>
+            <Outfit image_url={item.image_url} outfit_name={item.outfit_name} />
+          </Link>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
