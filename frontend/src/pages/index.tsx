2cabd9105dae7e4d65c47bf004a0351aa0a58fba
KhoiNguyen <thorwaitson@gmail.com>
2022-11-02 00:51:12 -0500

    update Calendar + add useState

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Calendar } from "react-calendar";
 import { Link } from "react-router-dom";
 import Outfit from "../components/Outfit";
@@ -12,30 +13,58 @@ type Outfit = {
   last_worn: Date;
 };
 
-const outfits: Outfit[] = [
+const outfitsJson: Outfit[] = [
+  {
+    id: "qwer",
+    image_url:
+      "https://theimpression.com/wp-content/uploads/2019/03/celine-rf19-1201.jpg",
+    label: "Black",
+    details: ["hat", "black"],
+    last_worn: new Date("2022-10-31T12:00"),
+  },
   {
     id: "fsafds",
     image_url:
       "https://www.boredpanda.com/blog/wp-content/uploads/2022/03/horrible-dresses-25-622749b4cfad9__700.jpg",
     label: "sun god",
     details: ["sun", "black"],
-    last_worn: new Date("2022-10-31"),
+    last_worn: new Date("2022-11-01T12:00"),
+  },
+  {
+    id: "al",
+    image_url:
+      "https://assets.vogue.com/photos/5d77fd188d44ee0009799518/master/pass/00001-Cong-Tri-Ready-To-Wear-Spring-2020.jpg",
+    label: "White dress",
+    details: ["pink", "umbrella"],
+    last_worn: new Date("2022-11-01T12:30"),
   },
   {
     id: "asdfdasf",
     image_url:
       "https://i.pinimg.com/originals/6a/c3/fe/6ac3fe0fcc2108847c9e107a5b2305cd.jpg",
-    label: "What?",
+    label: "Pink dress",
     details: ["pink", "umbrella"],
     last_worn: new Date(),
   },
 ];
 
 const Index = () => {
+  const [date, handleCalendarChange] = useState(new Date());
+
+  const [outfits, setOutfits] = useState(outfitsJson);
+  useEffect(() => {
+    setOutfits(
+      outfitsJson.filter(
+        (item) =>
+          item.last_worn.toLocaleDateString() === date.toLocaleDateString()
+      )
+    );
+  }, [date]);
+
   return (
     <Layout>
       <div className={homeStyle.container}>
-        <Calendar />
+        <Calendar onChange={handleCalendarChange} value={date} />
       </div>
 
       <div className={homeStyle.container}>
@@ -55,17 +84,24 @@ const Index = () => {
           Outfits you wore:
         </h2>
 
-        <div className={homeStyle.outfit_container}>
-          {outfits.map((item) => (
-            <Link to={`/${item.id}`} key={item.id}>
-              <Outfit
-                image_url={item.image_url}
-                outfit_name={item.label}
-                last_worn={item.last_worn}
-              />
-            </Link>
-          ))}
-        </div>
+        {outfits.length > 0 && (
+          <div className={homeStyle.outfit_container}>
+            {outfits.map((item) => (
+              <Link to={`/${item.id}`} key={item.id}>
+                <Outfit
+                  image_url={item.image_url}
+                  outfit_name={item.label}
+                  last_worn={item.last_worn}
+                />
+              </Link>
+            ))}
+          </div>
+        )}
+        {outfits.length === 0 && (
+          <p style={{ fontSize: "1.5rem", textAlign: "center" }}>
+            No outfits wore on this day
+          </p>
+        )}
       </div>
     </Layout>
   );
