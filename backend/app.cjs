62c5f077d4678a433d35d58d0e7ba5eb7fba2a34
KhoiNguyen <thorwaitson@gmail.com>
2022-11-03 15:42:22 -0500

    update folder structure

---
+++ backend/app.cjs
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const OutfitUtil = require("./utils/OutfitUtil.cjs");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "..", "frontend/dist")));
+
+app.get("/api/outfit", async (req, res) => {
+  const date = req.query.date;
+
+  const outfits = await OutfitUtil.get(date);
+
+  res.json({
+    success: outfits,
+    error: false,
+  });
+});
+
+// Express listening
+app.listen(PORT, () =>
+  console.log(`Server is running: http://localhost:${PORT}/`)
+);
---
+++ backend/utils/OutfitUtil.cjs
@@ -0,0 +1,41 @@
+//Reference to MongoDB connection at ../db/client
+const client = require("../db/client.cjs");
+
+module.exports = class OutfitUtil {
+  static async get(date) {
+    try {
+      const collection = client.db("wornout").collection("outfits");
+
+      const filter = { last_worn: new Date(date) };
+
+      const outfits = collection.find(filter).toArray();
+      if (!outfits) return false;
+
+      return outfits;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  // static async create(outfitData) {
+  //   try {
+  //     //inserts outfit Data
+  //     outfitData.image_url = outfitData.image_url.trim();
+  //     outfitData.label = outfitData.label.trim();
+  //     outfitData.details = outfitData.details.trim();
+  //     outfitData.last_worn = outfitData.last_worn.trim();
+
+  //     const collection = client.db("wornout").collection("outfits");
+
+  //     const response = await collection.insertOne(outfitData);
+
+  //     if (!response) throw new Error("Error saving outfit");
+
+  //     return true;
+  //   } catch (error) {
+  //     console.error(error);
+  //     return false;
+  //   }
+  // }
+};
