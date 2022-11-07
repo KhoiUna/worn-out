2f626f6f348621464e5a5cc0e618e41d21ee3c0e
KhoiNguyen <thorwaitson@gmail.com>
2022-11-07 16:23:59 -0600

    uninstall react-calendar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import "react-calendar/dist/Calendar.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Index from "./pages";
