bc21f6fc56ddb2c6e4ee43b22d06dccee1006e93
KhoiNguyen <thorwaitson@gmail.com>
2022-11-07 15:34:14 -0600

    backend: update OutfitUtil.cjs

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
 const app = express();
-
 const OutfitUtil = require("./utils/OutfitUtil.cjs");
 
 const PORT = process.env.PORT || 3000;
 
 // Serve the built React frontend
 app.use(express.static(path.join(__dirname, "..", "frontend/dist")));
+
+// Initialize server & middleware
+app.use(express.json());
 
 app.get("/api/outfit", async (req, res) => {
   const date = req.query.date;
@@ -17,6 +18,48 @@ app.get("/api/outfit", async (req, res) => {
 
   res.json({
     success: outfits,
+    error: false,
+  });
+});
+
+app.get("/api/outfit/:outfitID", async (req, res) => {
+  const outfit = await OutfitUtil.getOne(req.params.outfitID);
+
+  res.json({
+    success: outfit,
+    error: false,
+  });
+});
+
+app.post("/api/outfit/save", async (req, res) => {
+  const response = await OutfitUtil.saveOutfit(req.body);
+
+  if (!response) console.error(response);
+
+  response.json({
+    success: true,
+    error: false,
+  });
+});
+
+app.post("/api/outfit/delete/:outfitID", async (req, res) => {
+  const response = await OutfitUtil.deleteUser(req.params.outfitID);
+
+  if (!response) console.error(response);
+
+  response.json({
+    success: true,
+    error: false,
+  });
+});
+
+app.post("/api/outfit/update/:outfitID", async (req, res) => {
+  const response = await OutfitUtil.updateOne(req.params.outfitID, req.body);
+
+  if (!response) console.error(response);
+
+  response.json({
+    success: true,
     error: false,
   });
 });
--- backend/utils/OutfitUtil.cjs
+++ backend/utils/OutfitUtil.cjs
@@ -18,24 +18,91 @@ module.exports = class OutfitUtil {
     }
   }
 
-  // static async create(outfitData) {
-  //   try {
-  //     //inserts outfit Data
-  //     outfitData.image_url = outfitData.image_url.trim();
-  //     outfitData.label = outfitData.label.trim();
-  //     outfitData.details = outfitData.details.trim();
-  //     outfitData.last_worn = outfitData.last_worn.trim();
+  static async getOne(outfitId) {
+    try {
+      const collection = client.db("wornout").collection("outfits");
 
-  //     const collection = client.db("wornout").collection("outfits");
+      const filter = { _id: outfitId };
 
-  //     const response = await collection.insertOne(outfitData);
+      const outfit = collection.find(filter).toArray();
+      if (!outfit) return false;
 
-  //     if (!response) throw new Error("Error saving outfit");
+      return outfit;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
 
-  //     return true;
-  //   } catch (error) {
-  //     console.error(error);
-  //     return false;
-  //   }
-  // }
+  // Inserts outfit Data
+  static async saveOutfit(outfitData) {
+    try {
+      outfitData.image_url = outfitData.image_url.trim();
+      outfitData.label = outfitData.label.trim();
+      outfitData.details = outfitData.details.trim();
+      outfitData.last_worn = outfitData.last_worn.trim();
+
+      const collection = client.db("wornout").collection("outfits");
+
+      const response = await collection.insertOne(outfitData);
+
+      if (!response) throw new Error("Error saving outfit");
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  static async deleteOutfit(outfitId) {
+    try {
+      // Connect to a collection using the client connection object created in ../db/client.js
+      const collection = client.db("wornout").collection("outfits");
+
+      // Use this collection to delete one user if the _id in Mongo matches the userId from request
+      const response = await collection.deleteOne({
+        _id: ObjectId(outfitId),
+      });
+
+      // Throw an error if there is no response
+      if (!response) throw new Error("Error deleting outfit");
+
+      // Return true if succeeded
+      return true;
+    } catch (error) {
+      // Error thrown above will be shown here
+      console.error(error);
+
+      // Return false if failed
+      return false;
+    }
+  }
+
+  static async updateOne(outfitId, outfitData) {
+    try {
+      const collection = client.db("wornout").collection("outfits");
+
+      const filter = { _id: ObjectId(outfitId) }; // pass in outfitId
+
+      // update the key 'label' with the new name from userData
+      const updateDoc = {
+        $set: {
+          image_url: outfitData.image_url,
+          label: outfitData.label,
+          details: outfitData.details,
+          last_worn: outfitData.last_worn,
+        },
+      };
+
+      const response = await collection.updateOne(filter, updateDoc);
+
+      if (!response) throw new Error("Error deleting outfit");
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
 };
