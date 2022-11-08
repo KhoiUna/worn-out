5cd7bb048ed0bc48a3335d823b13b99fcb2f25fe
KhoiNguyen <thorwaitson@gmail.com>
2022-11-08 13:58:34 -0600

    frontend: update buttons border

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages";
 import View from "./pages/[id]";
 import Add from "./pages/add";
