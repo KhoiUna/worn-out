dd758fc7f491ea1d853b4b2d8bd6940213171b70
KhoiNguyen <thorwaitson@gmail.com>
2022-11-14 14:56:17 -0600

    frontend: update home page UI

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,144 +1,22 @@
-import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import Outfit from "../components/Outfit";
 import Layout from "../containers/Layout";
-import homeStyle from "../styles/home.module.css";
-import axios from "axios";
-
-type Outfit = {
-  _id: string;
-  image_url: string;
-  label: string;
-  details: string[];
-  last_worn: Date;
-};
+import homePageStyle from "../styles/home.module.css";
 
 const Index = () => {
-  const [date, setDate] = useState("");
-
-  const { isLoading: uniqueDateIsLoading, data: uniqueDates } = useQuery<
-    any,
-    any,
-    string[],
-    any
-  >({
-    queryKey: ["uniqueDates"],
-    queryFn: () =>
-      axios.get("/api/uniqueDates").then((res) => res.data.success),
-  });
-
-  const { isLoading, data: fetchedOutfits } = useQuery<any, any, Outfit[], any>(
-    {
-      queryKey: ["fetchedOutfits", date],
-      queryFn: () =>
-        axios.get(`/api/outfit?date=${date}`).then((res) => res.data.success),
-    }
-  );
-
-  const handleDateChange = (event: SyntheticEvent) => {
-    setDate((event.target as HTMLSelectElement).value);
-  };
-
-  if (isLoading || !fetchedOutfits || uniqueDateIsLoading || !uniqueDates)
-    return (
-      <Layout>
-        <div className={homeStyle.container}>
-          <Link to={"/add"}>
-            <button className={homeStyle.add_outfit_button}>
-              Add an outfit
-            </button>
-          </Link>
-        </div>
-
-        <div className={homeStyle.container}>
-          <select className={homeStyle.dropdown} defaultValue={""} disabled>
-            <option value="" disabled>
-              Loading...
-            </option>
-          </select>
-        </div>
-
-        <div
-          style={{
-            backgroundColor: "#f1f1f1",
-            padding: "0.5rem",
-            borderRadius: "10px",
-            margin: "auto",
-            textAlign: "center",
-          }}
-        >
-          <h2 style={{ padding: "0.5rem", fontSize: "1.5rem" }}>
-            Outfits you wore:
-          </h2>
-
-          <h2>Loading...</h2>
-        </div>
-      </Layout>
-    );
-
   return (
     <Layout>
-      <div className={homeStyle.container}>
-        <Link to={"/add"}>
-          <button className={homeStyle.add_outfit_button}>Add an outfit</button>
-        </Link>
-      </div>
-
-      <div className={homeStyle.container}>
-        <select
-          className={homeStyle.dropdown}
-          value={date}
-          onChange={handleDateChange}
-        >
-          <option value="" disabled>
-            {uniqueDates.length > 0
-              ? "Please select a date"
-              : "No outfits added"}
-          </option>
-          {uniqueDates.map((date) => (
-            <option key={date} value={date}>
-              {new Date(date).toLocaleDateString()}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      <div>
-        {date && (
-          <div
-            style={{
-              backgroundColor: "#f1f1f1",
-              padding: "0.5rem",
-              borderRadius: "10px",
-              margin: "auto",
-              textAlign: "center",
-            }}
-          >
-            <h2 style={{ padding: "0.5rem", fontSize: "1.5rem" }}>
-              Outfits you wore:
-            </h2>
-
-            {fetchedOutfits.length > 0 && (
-              <div className={homeStyle.outfit_container}>
-                {fetchedOutfits.map((item) => (
-                  <Link to={`/view/${item._id}`} key={item._id}>
-                    <Outfit
-                      image_url={item.image_url}
-                      outfit_name={item.label}
-                      last_worn={item.last_worn}
-                    />
-                  </Link>
-                ))}
-              </div>
-            )}
-            {fetchedOutfits.length === 0 && (
-              <p style={{ fontSize: "1.5rem", textAlign: "center" }}>
-                No outfits wore on this day
-              </p>
-            )}
+      <div className={homePageStyle.container}>
+        <Link to={"/my-outfits"}>
+          <div className={homePageStyle.button_container}>
+            <button className={homePageStyle.button}>My Outfits</button>
           </div>
-        )}
+        </Link>
+
+        <Link to={"/add"}>
+          <div className={homePageStyle.button_container}>
+            <button className={homePageStyle.button}>New Fit</button>
+          </div>
+        </Link>
       </div>
     </Layout>
   );
